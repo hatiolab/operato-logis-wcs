f9129969f9ce75e744d86194674d40e3c4ec43e1
wryang <yang@yang-mac.local>
2021-01-21 11:38:22 +0900

    정리..

--- src/main/java/operato/logis/wcs/job/EquipProductivityService.java
+++ src/main/java/operato/logis/wcs/job/EquipProductivityService.java
@@ -33,59 +33,52 @@ public class EquipProductivityService {
 	@Scheduled(fixedDelay=30000)
 	@Transactional
 	public void summaryProductivity() {
+		// 1. 대상 작업 배치 조회 ( 상태만 진행중, 마감 인 것 들 )
+		// TODO 마감 배치인경우 작업 일자 로 생산성 집계 구간 정의 필요
+		Query condition = new Query();
+		condition.addFilter("status", OrmConstants.IN, ValueUtil.newStringList(JobBatch.STATUS_END, JobBatch.STATUS_RUNNING));
+		List<JobBatch> runBatchList = this.queryManager.selectList(JobBatch.class, condition);
 		
-		//* 2,12,25,32,42,52 * * * ?
-		
-		this.logger.info("summaryProductivity : " + AnyDateUtil.currentTimeStr());
-		
-		try {
-			// 1. 대상 작업 배치 조회 ( 상태만 진행중인 것 들 )
-			Query condition = new Query();
-			condition.addFilter("status", OrmConstants.IN, ValueUtil.newStringList(JobBatch.STATUS_END, JobBatch.STATUS_RUNNING));
-			List<JobBatch> runBatchList = this.queryManager.selectList(JobBatch.class, condition);
+		// 2. Loop 대상 작업 배치 
+		for (JobBatch jobBatch : runBatchList ) {
+			// 2.1. 대상 작업 배치 PRODUCTIVITY upsert
+			Date instructedAt = jobBatch.getInstructedAt();
+			List<Date> prodDateList = null;
 			
-			// 2. Loop 대상 작업 배치 
-			for (JobBatch jobBatch : runBatchList ) {
-				// -- 작업 진행중인 상황에도 10분별 생산성과 시간별 생산성 데이터는 계속 update 한다. 
-				// -- 작업 대상 배치가 작업 지시한 시점 부터 데이터를 생성한다. 
-				
-				// 2.1. 대상 작업 배치 PRODUCTIVITY upsert
-				Date instructedAt = jobBatch.getInstructedAt();
-				List<Date> prodDateList = null;
-				
-				if(ValueUtil.isEqualIgnoreCase(jobBatch.getStatus(), JobBatch.STATUS_END)){
-					prodDateList = this.getIntervalTenMin(instructedAt, jobBatch.getFinishedAt());
-				} else {
-					prodDateList = this.getIntervalTenMin(instructedAt);
-				}
-
-				Productivity prod = null;
-				for(Date currentDate : prodDateList) {
-					prod = this.getProductivity(prod, jobBatch, currentDate);
-					
-					if(this.isCreateProductivityData(prod, currentDate)) {
-						prod = this.createProductivity(jobBatch, currentDate);
-					}
-					
-					int baseMin = AnyDateUtil.minInt(currentDate) + 10;
-					
-					if(ValueUtil.isNotEmpty(ClassUtil.getFieldValue(prod, "m" + baseMin + "ResultStr"))){
-						continue;
-					}
-					
-					prod = this.updateProductivity(prod, "m" + baseMin + "Result", 100);
-				}
-
-				// 2.2. 대상 작업 배치 DAILY_PROD_SUMMARY	 upsert
-				
+			if(ValueUtil.isEqualIgnoreCase(jobBatch.getStatus(), JobBatch.STATUS_END)){
+				prodDateList = this.getIntervalTenMin(instructedAt, jobBatch.getFinishedAt());
+			} else {
+				prodDateList = this.getIntervalTenMin(instructedAt);
 			}
-			
-		}catch (Exception e) {
-			this.logger.error("ASB", e);
+
+			Productivity prod = null;
+			for(Date currentDate : prodDateList) {
+				prod = this.getProductivity(prod, jobBatch, currentDate);
+				
+				if(this.isCreateProductivityData(prod, currentDate)) {
+					prod = this.createProductivity(jobBatch, currentDate);
+				}
+				
+				int baseMin = AnyDateUtil.minInt(currentDate) + 10;
+				
+				if(ValueUtil.isNotEmpty(ClassUtil.getFieldValue(prod, "m" + baseMin + "ResultStr"))){
+					continue;
+				}
+				
+				prod = this.updateProductivity(prod, "m" + baseMin + "Result", 100);
+			}
+
+			// 2.2. 대상 작업 배치 DAILY_PROD_SUMMARY	 upsert
 			
 		}
 	}
 	
+	/**
+	 * 10분별 생산성 데이터 생성 
+	 * @param jobBatch
+	 * @param date
+	 * @return
+	 */
 	private Productivity createProductivity(JobBatch jobBatch, Date date) {
 		Productivity prod = new Productivity();
 		
@@ -108,6 +101,13 @@ public class EquipProductivityService {
 		return this.queryManager.insert(Productivity.class, prod);
 	}
 	
+	/**
+	 * 10분별 생산성 컬럼 update 
+	 * @param prod
+	 * @param fieldName
+	 * @param fieldValue
+	 * @return
+	 */
 	private Productivity updateProductivity(Productivity prod, String fieldName, Object fieldValue) {
 		ClassUtil.setFieldValue(prod, fieldName, fieldValue);
 		this.queryManager.update(prod, fieldName, "updatedAt");
@@ -189,11 +189,12 @@ public class EquipProductivityService {
 		while(true) {
 			Date lastDate = dateList.get(dateList.size()-1);
 			
+			// 4.1 종료 시간과 잊전 시간 값이 같으면 Loop 종료 
 			if(ValueUtil.isEqual(AnyDateUtil.dateTimeStr(lastDate),endDateStr)) {
 				break;
 			}
 			
-			// 3.1 
+			// 4.2 이전 시간 값에 10분 증가후 리스트에 젖장  
 			dateList.add(AnyDateUtil.addMinutes(lastDate, 10));
 		}
 		
@@ -206,12 +207,16 @@ public class EquipProductivityService {
 	 */
 	private Productivity getProductivity(Productivity prod, JobBatch jobBatch, Date date) {
 		
+		// 1. 기존 데이터와 같은 데이터를 사용하는 경우에는 기존 데이터 재사용 
 		if(ValueUtil.isNotEmpty(prod) 
+			&& ValueUtil.isEqual(prod.getBatchId(), jobBatch.getId()) 
 			&& ValueUtil.isEqual(prod.getJobDate(), AnyDateUtil.dateStr(date)) 
 			&& ValueUtil.isEqual(prod.getJobHour(), AnyDateUtil.hourStr(date))) {
 			return prod;
 		}
 		
+		// 2. 기존 10분별 집계 여부를 확인 하기위해 집계 값을 string 으로도 조회 
+		// null 여부로 해당 시간에 집계 대상 분을 구하기 위함 
 		String sql = "select x.*, m10_result as m10_result_str, m20_result as m20_result_str, m30_result as m30_result_str, m40_result as m40_result_str, m50_result as m50_result_str, m60_result as m60_result_str from productivity x where domain_id = :domainId and batch_id = :batchId and job_date = :jobDate and job_hour = :jobHour";
 		Map<String,Object> params = ValueUtil.newMap("domainId,batchId,jobDate,jobHour", jobBatch.getDomainId(),jobBatch.getId(),AnyDateUtil.dateStr(date),AnyDateUtil.hourStr(date));
 		
